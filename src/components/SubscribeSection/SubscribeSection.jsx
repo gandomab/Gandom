64eb8ffbd81b5f96984a5ceb0c4f1a6a633eda7e
Sneha Dileep <dileepsneha99@gmail.com>
2026-03-17 10:48:01 +0100

    updated img height

--- src/components/SubscribeSection/SubscribeSection.jsx
+++ src/components/SubscribeSection/SubscribeSection.jsx
@@ -10,7 +10,7 @@ const SubscribeSection = () => {
     // };
 
     return (
-        <section className="w-full flex h-[208px] md:h-[326px] lg:h-[532px] mt-16">
+        <section className="w-full flex h-[208px] md:h-[326px] lg:h-[463px] mt-16">
             <div className="relative w-full overflow-hidden">
 
                 {/* Background + Gradient */}
