fefd58e4b6ff9baba8490bb933835e20a33cc0a2
Sneha Dileep <dileepsneha99@gmail.com>
2026-05-15 12:44:11 +0200

    implemented choose your payment option section with icons

--- src/pages/Pay/PayPage.jsx
+++ src/pages/Pay/PayPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PayPageHeroSection from "../../components/PayPageHeroSection/PayPageHeroSection";
 import PaymentOrderSummary from "../../components/PaymentOrderSummarySection/PaymentOrderSummary";
+import PaymentOptions from "../../components/PaymentOptionsSection/PaymentOptions";
 
 const PayPage = () => {
     return (
@@ -9,7 +10,7 @@ const PayPage = () => {
             <section className="mt-4 mb-4">
                 <PayPageHeroSection />
             </section>
-            <section className="max-w-7xl mx-auto px-4 mt-8 mb-4">
+            <section className="max-w-7xl mx-auto px-4 mt-8 mb-10">
                 <h2 className="font-inter font-bold text-[#E6B220] text-[13px] md:text-[32px] xl:text-[64px] leading-[130%] mb-4">Review and Order</h2>
                 <div className="grid grid-cols-1 md:grid-cols-12 gap-8">
                     {/* LEFT COLUMN: Your Order Summary */}
@@ -18,9 +19,7 @@ const PayPage = () => {
                     </div>
                     {/* RIGHT COLUMN: Payment Options */}
                     <div className="md:col-span-5 xl:col-span-6">
-                        <div className="bg-white p-8 rounded-3xl border border-gray-100 shadow-sm">
-                            <h3 className="font-inter font-semibold text-[#000000] text-[20px] md:text-[20px] lg:text-[25px] xl:text-[36px] leading-[130%] mb-6">Choose your payment option</h3>
-                        </div>
+                        <PaymentOptions />
                     </div>
                 </div>
             </section>
