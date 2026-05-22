6d14ce2fd3c43698d5908e537e3dce4d73b37793
Sneha Dileep <dileepsneha99@gmail.com>
2026-05-22 10:14:30 +0200

    updated the text section on event page

--- src/pages/EventsPage/page.jsx
+++ src/pages/EventsPage/page.jsx
@@ -11,7 +11,7 @@ const EventsPage = () => {
       </section>
 
       {/* Events Cards Section */}
-      <section className="mt-10 mb-10">
+      <section className="mt-10">
         <div className="flex flex-col gap-4 md:gap-6 lg:gap-8 xl:gap-12 px-4 md:px-6 lg:px-12 xl:px-24 max-w-8xl mx-auto">
           {events.map((event, idx) => (
             <EventsCard
@@ -22,6 +22,15 @@ const EventsPage = () => {
               isSpecial={idx === 0 || idx === 1}
             />
           ))}
+        </div>
+      </section>
+
+      {/* Text section */}
+      <section className="mt-10 mb-10">
+        <div className="font-inter font-semibold text-[#1E1E1E] text-[12px] md:text-[16px] xl:text-[24px] leading-[150%] text-center px-4 md:px-6 lg:px-12 xl:px-24">
+          <p>You can place your order easily through our website.</p>
+          <p> During checkout, you'll have the option to add a specific message for your order.</p>
+          <p>If you prefer, you can also contact us directly and our team will be happy to assist you personally.</p>
         </div>
       </section>
     </div>
