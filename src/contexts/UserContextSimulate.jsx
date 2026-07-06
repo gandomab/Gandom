113571192e6c494b831171bd5a4d4880d6821307
Sneha Dileep <dileepsneha99@gmail.com>
2026-07-06 11:41:51 +0200

    removed comments

--- src/contexts/UserContextSimulate.jsx
+++ src/contexts/UserContextSimulate.jsx
@@ -1,5 +1,3 @@
-// this is a temporary file to simulate the user context
-// this file will be removed or updated when connected to the backend
 
 import { createContext, useContext, useState } from 'react';
 
